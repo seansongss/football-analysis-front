350dfd3eb7cf4bc8220a5e0d8a8619288bf72cb3
seansongss <seansongss@gmail.com>
2025-05-30 10:57:25 -0400

    setUpload and handluSubmit

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ import { useRouter } from 'next/navigation'
 import { FileForm } from '@/components/file-form'
 
 export default function HomePage() {
+  const [uploading, setUploading] = useState(false)
   const [file, setFile] = useState<File| null>(null)
   const router = useRouter()
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
+    console.log('Submitting file:', file)
     if (!file) return
     const form = new FormData()
     form.append('file', file)
+    setUploading(true)
     const res = await fetch('/api/process', {
       method: 'POST',
       body: form,
@@ -21,8 +24,15 @@ export default function HomePage() {
   }
 
   return (
-    <div className='flex-col max-w-1/3 items-center'>
-      <FileForm />
-    </div>
+    <>
+      <div className='flex-col max-w-1/3 items-center'>
+        <FileForm setFile={setFile} onSubmit={handleSubmit}  />
+      </div>
+      {uploading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="text-white">Uploading...</div>
+        </div>
+      )}
+    </>
   )
 }
